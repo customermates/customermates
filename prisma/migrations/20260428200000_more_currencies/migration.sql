62465a1948b901288a3fa8982efc4b4050c75b76
Benjamin Wagner <benjamin-wagner@mein.gmx>
2026-04-28 15:35:50 +0200

    feat(currencies): add new currencies and update translations
    - Added multiple new currencies (AED, CZK, DKK, HKD, HUF, ILS, KRW, MXN, NOK, NZD, PLN, SAR, SEK, SGD, TRY, TWD, ZAR) to the currency constants.
    - Updated German and English translations for the newly added currencies.
    - Modified the Prisma schema to include new currency values in the enum.
    - Created a migration script to alter the Currency enum in the database.

---
+++ prisma/migrations/20260428200000_more_currencies/migration.sql
@@ -0,0 +1,18 @@
+-- AlterEnum
+ALTER TYPE "Currency" ADD VALUE 'sek';
+ALTER TYPE "Currency" ADD VALUE 'nok';
+ALTER TYPE "Currency" ADD VALUE 'dkk';
+ALTER TYPE "Currency" ADD VALUE 'pln';
+ALTER TYPE "Currency" ADD VALUE 'czk';
+ALTER TYPE "Currency" ADD VALUE 'huf';
+ALTER TYPE "Currency" ADD VALUE 'try';
+ALTER TYPE "Currency" ADD VALUE 'twd';
+ALTER TYPE "Currency" ADD VALUE 'zar';
+ALTER TYPE "Currency" ADD VALUE 'mxn';
+ALTER TYPE "Currency" ADD VALUE 'krw';
+ALTER TYPE "Currency" ADD VALUE 'hkd';
+ALTER TYPE "Currency" ADD VALUE 'sgd';
+ALTER TYPE "Currency" ADD VALUE 'nzd';
+ALTER TYPE "Currency" ADD VALUE 'aed';
+ALTER TYPE "Currency" ADD VALUE 'sar';
+ALTER TYPE "Currency" ADD VALUE 'ils';
